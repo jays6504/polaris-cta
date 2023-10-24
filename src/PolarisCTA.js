03ff93bdb590842e17cb061ad8f398f737a9926a
Jay Stephens <143051188+jays6504@users.noreply.github.com>
2023-10-24 12:27:25 -0400

    Basic functionality
    Basic functionality

--- src/PolarisCTA.js
+++ src/PolarisCTA.js
@@ -3,7 +3,14 @@ import { LitElement, html, css } from 'lit';
 export class PolarisCTA extends LitElement {
   static get properties() {
     return {
-      title: { type: String },
+      name: { type: String },
+      link: {type: String},
+      light: {type: Boolean, reflect: true},
+      dark: {type: Boolean, reflect: true},
+      tinted: {type: Boolean, reflect: true},
+      beveled: {type: Boolean, reflect: true},
+      gradient: {type: Boolean, reflect: true},
+      active: {type: Boolean, reflect: true},
     };
   }
 
@@ -12,15 +19,54 @@ export class PolarisCTA extends LitElement {
       :host {
         display: block;
       }
+      /**
+      the following create the default state of the CTAs 
+       */
+      :host([light]) {
+        border: 1px solid #fff;
+      }
+      :host([dark]) {
+        background-color: #fff;
+      }
+      :host([tinted]) {
+        background-color: #fff;
+      }
+      :host([beveled]) {
+        border-radius: 4px;
+      }
+
+      /**
+      the following create the interacted versions default state of the CTAs 
+       */
+      :host([light]) .link:focus,
+      :host([light]) .link:hover,
+      :host([light][active]) .link{
+        background-color: #000;
+      }
+      :host([dark]) .link:focus,
+      :host([dark]) .link:hover,
+      :host([dark][active]) .link{
+        background-color: #000;
+      }
+      :host([gradient]) .link:focus,
+      :host([gradient]) .link:hover,
+      :host([gradient][active]) .link{
+        background:linear-gradient(#fff, #000);
+      }
     `;
   }
 
   constructor() {
     super();
-    this.title = 'My boilerplate';
+    this.name = 'My boilerplate';
+    this.light = false;
+    this.dark = false;
+    this.tinted = false;
+    this.beveled = true;
+    this.gradient = false;
   }
 
   render() {
-    return html`<span>${this.title}</span>`;
+    return html`<a class='link' href='${this.link}'><span>${this.name} ></span></a>`;
   }
 }
